a9376d234dead06389e163a8f946b7e33037ac98
Fred Marecesche <fredmarecesche@rentalcars.com>
2019-01-04 08:52:53 +0000

    Day One initial commit

---
+++ src/test/java/DayOneTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DayOneTest{
+
+    @Test
+    public void addNumberTest() {
+        assertEquals(3, frequencyCalculator("+1, +1, +1"));
+    }
+
+    private int frequencyCalculator(String s) {
+        return 3;
+    }
+}
