5aba460843d68b7ef84eec5c26412052c4997729
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-03-14 13:53:29 +0000

    AdventOfCalendar Day 2

--- src/main/java/DayOne.java
+++ src/main/java/DayOne.java
@@ -4,7 +4,6 @@ import java.util.stream.IntStream;
 public class DayOne {
 
     public int frequencyCalculator(String s) {
-        int[] inputFrequencies = Arrays.stream(s.split(", ")).mapToInt(Integer::parseInt).toArray();
-        return IntStream.of(inputFrequencies).sum();
+        return IntStream.of(Arrays.stream(s.split(", ")).mapToInt(Integer::parseInt).toArray()).sum();
     }
 }
