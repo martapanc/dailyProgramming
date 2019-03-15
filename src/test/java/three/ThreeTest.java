e86c45b4f34f2b7026bb7e27fc646139aa1cb967
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-03-15 11:35:39 +0000

    Day 3 - add tests from example

--- src/test/java/three/ThreeTest.java
+++ src/test/java/three/ThreeTest.java
@@ -20,6 +20,20 @@ public class ThreeTest {
     }
 
     @Test
+    public void test_compute_occupied_inches_2(){
+        List<String> expCoordinates = new ArrayList<>();
+        expCoordinates.add("3,4");
+        assertEquals(expCoordinates, Three.computeOccupiedInches(2, 3, 1, 1));
+    }
+
+    @Test
+    public void test_compute_occupied_inches_3(){
+        List<String> expCoordinates = new ArrayList<>();
+        expCoordinates.add("2,2");
+        assertEquals(expCoordinates, Three.computeOccupiedInches(1, 1, 1, 1));
+    }
+
+    @Test
     public void test_compute_occupied_inches_4(){
         List<String> expCoordinates = new ArrayList<>();
         expCoordinates.add("2,3");
@@ -32,16 +46,34 @@ public class ThreeTest {
     }
 
     @Test
-    public void test_compute_occupied_inches_1(){
+    public void test_compute_occupied_inches_5(){
         List<String> expCoordinates = new ArrayList<>();
+        expCoordinates.add("2,4");
+        expCoordinates.add("2,5");
+        expCoordinates.add("2,6");
+        expCoordinates.add("2,7");
         expCoordinates.add("3,4");
-        assertEquals(expCoordinates, Three.computeOccupiedInches(2, 3, 1, 1));
+        expCoordinates.add("3,5");
+        expCoordinates.add("3,6");
+        expCoordinates.add("3,7");
+        expCoordinates.add("4,4");
+        expCoordinates.add("4,5");
+        expCoordinates.add("4,6");
+        expCoordinates.add("4,7");
+        expCoordinates.add("5,4");
+        expCoordinates.add("5,5");
+        expCoordinates.add("5,6");
+        expCoordinates.add("5,7");
+        assertEquals(expCoordinates, Three.computeOccupiedInches(1, 3, 4, 4));
     }
 
     @Test
-    public void test_compute_occupied_inches_2(){
+    public void test_compute_occupied_inches_6(){
         List<String> expCoordinates = new ArrayList<>();
+        expCoordinates.add("1,1");
+        expCoordinates.add("1,2");
+        expCoordinates.add("2,1");
         expCoordinates.add("2,2");
-        assertEquals(expCoordinates, Three.computeOccupiedInches(1, 1, 1, 1));
+        assertEquals(expCoordinates, Three.computeOccupiedInches(0, 0, 2, 2));
     }
 }
