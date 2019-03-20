b0c12ac052dd509511a1eca2966dd8466a1462f4
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-03-20 16:45:46 +0000

    Day 4: add tests

--- src/test/java/four/FourTest.java
+++ src/test/java/four/FourTest.java
@@ -2,6 +2,10 @@ package four;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FourTest {
 
     //Sort entries by date and time
@@ -11,8 +15,18 @@ public class FourTest {
     @Test
     public void test_read_input_and_sort() {
 
-        Four.sortInputByDate(Four.readInputAndParse());
+    }
 
+    @Test
+    public void test_get_guard_with_most_time_asleep(){
+        List<GuardStatus> list = Four.sortInputByDate(Four.readInputAndParse("src/main/java/four/out2"));
+        assertEquals(10, Four.getGuardWithMostTimeAsleep(list));
+    }
+
+    @Test
+    public void test_get_guard_with_most_time_asleep_2(){
+        List<GuardStatus> list = Four.sortInputByDate(Four.readInputAndParse("src/main/java/four/out"));
+        assertEquals(3119, Four.getGuardWithMostTimeAsleep(list));
     }
 
 }
