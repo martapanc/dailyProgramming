a9dfc03846a487518cfefbafebed6cc98ee2a916
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-04-02 17:26:03 +0100

    Day 5 - part 2

--- src/test/java/five/FiveTest.java
+++ src/test/java/five/FiveTest.java
@@ -2,6 +2,9 @@ package five;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FiveTest {
@@ -23,6 +26,7 @@ public class FiveTest {
         String input = Five.readInputFile("src/main/java/five/in2");
         assertEquals(10638, Five.computeAlchemicalReductionLength(input));
     }
+
     @Test
     public void test_compute_alchemical_reduction_3() {
         String input = Five.readInputFile("src/main/java/five/in3");
@@ -37,5 +41,18 @@ public class FiveTest {
     @Test
     public void test_remove_all_letter() {
         assertEquals("dabCBAcaDA", Five.removeAllLetter("daFbCBfAcaFfDA", "f"));
+    }
+
+    @Test
+    public void test_compute_minimum_alchemical_reduction() {
+        String input = Five.readInputFile("src/main/java/five/in2");
+
+        List<Integer> results = new ArrayList<>();
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            results.add(Five.computeAlchemicalReductionLength(Five.removeAllLetter(input, letter + "")));
+        }
+
+        System.out.println(results);
     }
 }
