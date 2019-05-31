980d1bd53187c13720816fd5cbe41c0b1a260a61
Marta Pancaldi <marta.panc@gmail.com>
2019-05-31 19:23:35 +0100

    Polish notation calculator

--- src/main/java/codewars/polish/PolishNotationCalculator.java
+++ src/main/java/codewars/polish/PolishNotationCalculator.java
@@ -1,13 +1,72 @@
 package codewars.polish;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public class PolishNotationCalculator {
 
-
     public double evaluate(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        List<String> list = Arrays.stream(s.split(" ")).collect(Collectors.toList());
 
-        double sum = 0;
+        while (list.size() > 1) {
+            for (int i = 0; i < list.size() - 2; i++) {
+                try{
+                    double first = Double.parseDouble(list.get(i));
+                    double second = Double.parseDouble(list.get(i + 1));
+                    String op = list.get(i + 2);
+                    double result;
+                    switch (op) {
+                        case "+":
+                            result = first + second; break;
+                        case "-":
+                            result = first - second; break;
+                        case "*":
+                            result = first * second; break;
+                        case "/":
+                            result = first / second; break;
+                        default:
+                            continue;
+                    }
 
+                    list.remove(i + 2);
+                    list.remove(i + 1);
+                    list.remove(i);
+                    list.add(i, result + "");
+                    break;
+                }
+                catch (NumberFormatException e) {
+                    continue;
+                }
+            }
+        }
+        return Double.parseDouble(list.get(0));
+    }
 
-        return sum;
+    public double evaluateWithStack(String expr) {
+        if (expr.equals("")) {
+            return 0;
+        }
+
+        Stack<Double> stack = new Stack<Double>();
+        String[] atoms = expr.split(" ");
+
+        for (String atom: atoms) {
+            Double a, b;
+            switch (atom) {
+                case "+": stack.push(stack.pop() + stack.pop()); break;
+                case "-": b = stack.pop(); a = stack.pop(); stack.push(a - b); break;
+                case "*": stack.push(stack.pop() * stack.pop()); break;
+                case "/": b = stack.pop(); a = stack.pop(); stack.push(a / b); break;
+                default:
+                    stack.push(Double.parseDouble(atom));
+            }
+        }
+
+        return stack.pop();
     }
 }
--- src/test/java/codewars/polish/PolishNotationCalculatorTest.java
+++ src/test/java/codewars/polish/PolishNotationCalculatorTest.java
@@ -9,36 +9,47 @@ class PolishNotationCalculatorTest {
 
     @Test
     public void shouldWorkWithEmptyString() {
-        assertEquals(0, calc.evaluate(""), 0);
+        assertEquals(0, calc.evaluate(""), 0.01);
     }
 
     @Test
     public void shouldParseNumbers() {
-        assertEquals(3, calc.evaluate("3"), 0);
+        assertEquals(3, calc.evaluate("3"), 0.01);
+        assertEquals(3, calc.evaluateWithStack("3"), 0.01);
     }
 
     @Test
     public void shouldParseFloatNumbers() {
-        assertEquals(3.5, calc.evaluate("3.5"), 0);
+        assertEquals(3.5, calc.evaluate("3.5"), 0.01);
+        assertEquals(3.5, calc.evaluate("3.5"), 0.01);
     }
 
     @Test
     public void shouldSupportAddition() {
-        assertEquals(4, calc.evaluate("1 3 +"), 0);
+        assertEquals(4, calc.evaluate("1 3 +"), 0.01);
+        assertEquals(4, calc.evaluateWithStack("1 3 +"), 0.01);
     }
 
     @Test
     public void shouldSupportMultiplication() {
-        assertEquals( 3, calc.evaluate("1 3 *"), 0);
+        assertEquals( 3, calc.evaluateWithStack("1 3 *"), 0.01);
     }
 
     @Test
     public void shouldSupportSubstraction() {
-        assertEquals(-2, calc.evaluate("1 3 -"), 0);
+        assertEquals(-2, calc.evaluate("1 3 -"), 0.01);
+        assertEquals(-2, calc.evaluateWithStack("1 3 -"), 0.01);
     }
 
     @Test
     public void shouldSupportDivision() {
-        assertEquals(2, calc.evaluate("4 2 /"), 0);
+        assertEquals(2, calc.evaluate("4 2 /"), 0.01);
+        assertEquals(2, calc.evaluateWithStack("4 2 /"), 0.01);
+    }
+
+    @Test
+    public void shouldSupportEverything(){
+        assertEquals(14, calc.evaluate("5 1 2 + 4 * + 3 -"), 0.01);
+        assertEquals(14, calc.evaluateWithStack("5 1 2 + 4 * + 3 -"), 0.01);
     }
 }
