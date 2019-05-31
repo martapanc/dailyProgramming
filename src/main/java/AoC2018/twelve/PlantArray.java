2401d2905dc2d10228a0625d53eeed5aa7c3c9bd
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-05-31 17:31:51 +0100

    AoC 2018 D12, part 2 plantarray

--- src/main/java/AoC2018/twelve/PlantArray.java
+++ src/main/java/AoC2018/twelve/PlantArray.java
@@ -8,11 +8,42 @@ public class PlantArray {
     private char[] plantArray;
     private int minNegativeIndex = -2;
 
+    private int shift = 0;
+
     /* Initialise array with two '.' at the beginning and at the end */
     public PlantArray(char[] array) {
-        this.plantArray = new char[array.length + 4];
-        IntStream.of(0, 1, this.plantArray.length - 2, this.plantArray.length - 1).forEach(i -> this.plantArray[i] = '.');
-        System.arraycopy(array, 0, this.plantArray, 2, array.length);
+
+        int emptyPotsAtTheBeginning = 0;
+        int emptyPotsAtTheEnd = 0;
+        if (array[0] == '#') {
+            emptyPotsAtTheBeginning = 2;
+        }
+        if (array[0] == '.' && array[1] == '#') {
+            emptyPotsAtTheBeginning = 1;
+        }
+        if (array[0] == '.' && array[1] == '.' && array[2] == '#') {
+            emptyPotsAtTheBeginning = 2;
+        }
+        if (array[array.length -1] == '#') {
+            emptyPotsAtTheEnd = 3;
+        }
+        if (array[array.length -2] == '#' && array[array.length -1] == '.') {
+            emptyPotsAtTheEnd = 2;
+        }
+        if (array[array.length -3] == '#' &&array[array.length -2] == '.' && array[array.length -1] == '.') {
+            emptyPotsAtTheEnd = 1;
+        }
+        this.plantArray = new char[array.length + emptyPotsAtTheBeginning + emptyPotsAtTheEnd];
+
+        for (int b = 0; b < emptyPotsAtTheBeginning; b ++) {
+            this.plantArray[b] = '.';
+        }
+        for (int e = 1; e <= emptyPotsAtTheEnd; e++) {
+            this.plantArray[plantArray.length - e] = '.';
+        }
+        this.shift = emptyPotsAtTheBeginning;
+//        IntStream.of(0, 1, this.plantArray.length - 2, this.plantArray.length - 1).forEach(i -> this.plantArray[i] = '.');
+        System.arraycopy(array, 0, this.plantArray, emptyPotsAtTheBeginning, array.length);
     }
 
     public PlantArray(PlantArray oldGen){
