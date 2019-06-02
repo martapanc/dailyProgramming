b77f49d5e81ddf5a6bb6c2c76742738ed64906c1
Marta Pancaldi <marta.panc@gmail.com>
2019-06-02 13:17:34 +0100

    AoC2018-1: refactor part 1

--- src/main/java/AoC2018/one/DayOne.java
+++ src/main/java/AoC2018/one/DayOne.java
@@ -1,11 +1,34 @@
 package AoC2018.one;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 public class DayOne {
 
-    public int frequencyCalculator(String s) {
+    public static int frequencyCalculator(String s) {
         return IntStream.of(Arrays.stream(s.split(", ")).mapToInt(Integer::parseInt).toArray()).sum();
+    }
+
+    public static String readInput(String inputRulesFile) {
+
+        String formattedInput = "";
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(inputRulesFile));
+            String line = reader.readLine();
+            while (line != null) {
+                formattedInput += line + ", ";
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return formattedInput;
     }
 }
--- src/test/java/AoC2018/one/DayOneTest.java
+++ src/test/java/AoC2018/one/DayOneTest.java
@@ -6,12 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class DayOneTest{
 
-    private DayOne dayOne = new DayOne();
+    public static final String INPUT1 = "src/test/java/AoC2018/one/input1";
+    public static final String INPUT2 = "src/test/java/AoC2018/one/input2";
 
     @Test
-    public void addNumberTest() {
-        assertEquals(3, dayOne.frequencyCalculator("+1, +1, +1"));
-        assertEquals(0, dayOne.frequencyCalculator("+1, +1, -2"));
+    public void test_addNumberTest() {
+        assertEquals(3, DayOne.frequencyCalculator("+1, +1, +1"));
+        assertEquals(0, DayOne.frequencyCalculator("+1, +1, -2"));
+        assertEquals(536, DayOne.frequencyCalculator(DayOne.readInput(INPUT1)));
     }
 
+    @Test
+    public void test_readInput() {
+        assertEquals("+19, -15, +6, +6, +17, -18, +7, +14, ", DayOne.readInput(INPUT2));
+    }
 }
