66da6b3f1ff62d64bc63cf003832fc9d4bf70c43
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-06-04 16:40:35 +0100

    AoC 2018 D15: find accessible points, reachable targets and closest target in reading order

--- src/main/java/AoC2018/fifteen/Fifteen.java
+++ src/main/java/AoC2018/fifteen/Fifteen.java
@@ -1,20 +1,11 @@
 package AoC2018.fifteen;
 
-import AoC2018.thirteen.Thirteen;
-
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Fifteen {
-
-    public static void fight() {
-        Unit elf1 = new Elf(new Point(1,1));
-        Unit goblin1 = new Goblin(new Point(2,3));
-
-        System.out.println(elf1.getAttackPoints());
-        System.out.println(elf1.getPosition());
-    }
 
     public static List<Unit> getInitialUnitPositions(char[][] matrix){
         List<Unit> unitList = new ArrayList<>();
@@ -28,22 +19,15 @@ public class Fifteen {
                 }
             }
         }
-
         return unitList;
     }
 
     public static List<Point> findPossibleTargets(Unit playingUnit, List<Unit> unitList, char[][] matrix) {
         List<Point> pointList = new ArrayList<>();
-        Class targetType;
-
-
-        if (playingUnit instanceof Elf) {
-            targetType = Goblin.class;
-        } else {
-            targetType = Elf.class;
-        }
+        Class targetType = playingUnit instanceof Elf ? Goblin.class : Elf.class;
 
         for (Unit potentialTarget : unitList) {
+            // Add potential targets of the 'enemy' category
             if (targetType.isInstance(potentialTarget)) {
                 Point[] pointArray = new Point[] {
                     new Point(potentialTarget.position.x, potentialTarget.position.y - 1),
@@ -58,14 +42,109 @@ public class Fifteen {
                         matrix[pointArray[3].y][pointArray[3].x]
                 };
                 for (int i = 0; i < pointArray.length; i++) {
-                    if (!pointList.contains(pointArray[i]) && charArray[i] != '#' && charArray[i] != 'E' && charArray[i] != 'G') {
+                    if (!pointList.contains(pointArray[i]) && charArray[i] == '.') {
                         pointList.add(pointArray[i]);
                     }
                 }
-
             }
         }
-
         return pointList;
+    }
+
+    public static List<Point> getAccessiblePoints(Unit playingUnit, char[][] matrix) {
+        int index = 0;
+        int maxX = matrix[0].length - 1, maxY = matrix.length - 1;
+        int x = playingUnit.position.x, y = playingUnit.position.y;
+        int[][] stack = new int[(maxX+1)*(maxY+1)][2];
+        char fillSymbol = '0', originalSymbol = '.';
+        List<Point> filledPoints = new ArrayList<>();
+
+        stack[0][1] = x;
+        stack[0][0] = y;
+        matrix[y][x] = fillSymbol;
+
+        while (index >= 0){
+            x = stack[index][1];
+            y = stack[index][0];
+            index--;
+
+            if ((x > 0) && (matrix[y][x-1] == originalSymbol)){
+                matrix[y][x-1] = fillSymbol;
+                filledPoints.add(new Point(x-1, y));
+                index++;
+                stack[index][1] = x-1;
+                stack[index][0] = y;
+            }
+            if ((x < maxX) && (matrix[y][x+1] == originalSymbol)){
+                matrix[y][x+1] = fillSymbol;
+                filledPoints.add(new Point(x+1, y));
+                index++;
+                stack[index][1] = x+1;
+                stack[index][0] = y;
+            }
+            if ((y > 0) && (matrix[y-1][x] == originalSymbol)){
+                matrix[y-1][x] = fillSymbol;
+                filledPoints.add(new Point(x, y-1));
+                index++;
+                stack[index][1] = x;
+                stack[index][0] = y-1;
+            }
+            if ((y < maxY) && (matrix[y+1][x] == originalSymbol)){
+                matrix[y+1][x] = fillSymbol;
+                filledPoints.add(new Point(x, y+1));
+                index++;
+                stack[index][1] = x;
+                stack[index][0] = y+1;
+            }
+        }
+        return filledPoints;
+    }
+
+    public static List<Point> findReachableTargets(Unit playingUnit, List<Unit> unitPositions, char[][] matrix) {
+        List<Point> possibleTargets = findPossibleTargets(playingUnit, unitPositions, matrix);
+        List<Point> allAccessiblePointsFromUnit = getAccessiblePoints(playingUnit, matrix);
+
+        List<Point> reachableTargets = new ArrayList<>();
+        // Find possible targets that are reachable
+        possibleTargets.forEach(pt -> allAccessiblePointsFromUnit.stream().filter(pt::equals).map(ap -> pt).forEach(reachableTargets::add));
+
+        return reachableTargets;
+    }
+
+    public static Point getClosestTargetInReadingOrder(Unit playingUnit, List<Point> reachableTargets) {
+        Map<Point, Integer> distanceMap = reachableTargets
+                .stream()
+                .collect(Collectors.toMap(target -> target, target -> getManhattanDistance(playingUnit.position, target), (a, b) -> b));
+        int minDistance = Collections.min(distanceMap.values());
+        return distanceMap
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue() == minDistance)
+                .map(Map.Entry::getKey)
+                .sorted(pointComparator())
+                .collect(Collectors.toList()).get(0);
+    }
+
+    public static Point getNextPosition(Unit playingUnit, Point target) {
+        //TODO: compute shortest distance
+        int currX = playingUnit.position.x, currY = playingUnit.position.y;
+        int newX = (currX == target.x) ? currX : ((currX < target.x) ? (currX + 1) : (currX - 1));
+        int newY = (currY == target.y) ? currY : ((currY < target.y) ? (currY + 1) : (currY - 1));
+
+        return new Point(newX, newY);
+    }
+
+    public static int getManhattanDistance(Point p1, Point p2) {
+        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+    }
+
+    private static Comparator<Point> pointComparator() {
+        Point p = new Point(0, -1000);
+        final Point finalP = new Point(p.x, p.y);
+        return (p0, p1) -> {
+            double ds0 = p0.distanceSq(finalP);
+            double ds1 = p1.distanceSq(finalP);
+            return Double.compare(ds0, ds1);
+        };
     }
 }
--- src/test/java/AoC2018/fifteen/FifteenTest.java
+++ src/test/java/AoC2018/fifteen/FifteenTest.java
@@ -8,20 +8,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FifteenTest {
 
     private final String input1 = "src/test/java/AoC2018/fifteen/input1";
+    private final String input_flood = "src/test/java/AoC2018/fifteen/input_flood";
 
     @Test
     public void testReadInputAndPrintMatrix(){
         Thirteen.printMatrix(Thirteen.readInput(input1, 9,9));
-    }
-
-    @Test
-    public void fight() {
-        Fifteen.fight();
     }
 
     @Test
@@ -54,5 +52,87 @@ class FifteenTest {
                 Fifteen.getInitialUnitPositions(Thirteen.readInput(input1, 9, 9)),
                 Thirteen.readInput(input1, 9, 9)
         ));
+    }
+
+    @Test
+    public void testGetAccessiblePoints() {
+        List<Point> accessiblePoints = new ArrayList<>();
+        accessiblePoints.add(new Point(2,1));
+        accessiblePoints.add(new Point(3,1));
+        accessiblePoints.add(new Point(1,2));
+        accessiblePoints.add(new Point(2,2));
+        accessiblePoints.add(new Point(3,2));
+        accessiblePoints.add(new Point(1,3));
+        accessiblePoints.add(new Point(2,3));
+        accessiblePoints.add(new Point(3,3));
+
+        assertThat("List equality without order",
+                Fifteen.getAccessiblePoints(new Goblin(new Point(1,1)), Thirteen.readInput(input_flood, 9,9)),
+                containsInAnyOrder(accessiblePoints.toArray()));
+                containsInAnyOrder(accessiblePoints.toArray());
+    }
+
+    @Test
+    public void testFindReachableTargets(){
+
+        List<Point> pointList = new ArrayList<>();
+        pointList.add(new Point(1, 5));
+        pointList.add(new Point(7, 5));
+        pointList.add(new Point(1, 6));
+        pointList.add(new Point(2, 7));
+        pointList.add(new Point(7, 6));
+        pointList.add(new Point(6, 7));
+
+        assertEquals(pointList, Fifteen.findReachableTargets(
+                new Elf(new Point(4,4)),
+                Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                Thirteen.readInput(input_flood, 9, 9)));
+
+        assertEquals(new ArrayList<>(), Fifteen.findReachableTargets(
+                new Goblin(new Point(1,1)),
+                Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                Thirteen.readInput(input_flood, 9, 9)));
+    }
+
+    @Test
+    public void testGetManhattanDistance() {
+        assertEquals(5, Fifteen.getManhattanDistance(new Point(1, 1), new Point(3, 4)));
+        assertEquals(4, Fifteen.getManhattanDistance(new Point(-1, -1), new Point(1, 1)));
+    }
+
+    @Test
+    public void testGetClosestTargetInReadingOrder() {
+        Unit e1 = new Elf(new Point(4,4));
+        assertEquals(new Point(1, 5), Fifteen.getClosestTargetInReadingOrder(e1,
+                Fifteen.findReachableTargets(e1,
+                        Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                        Thirteen.readInput(input_flood, 9, 9)))
+        );
+
+        Unit e2 = new Elf(new Point(4,7));
+        assertEquals(new Point(2, 7), Fifteen.getClosestTargetInReadingOrder(e2,
+                Fifteen.findReachableTargets(e2,
+                        Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                        Thirteen.readInput(input_flood, 9, 9)))
+        );
+
+        Unit e3 = new Elf(new Point(4,4));
+        assertEquals(new Point(4, 2), Fifteen.getClosestTargetInReadingOrder(e3,
+                Fifteen.findReachableTargets(e3,
+                        Fifteen.getInitialUnitPositions(Thirteen.readInput(input1, 9, 9)),
+                        Thirteen.readInput(input1, 9, 9)))
+        );
+
+        Unit g1 = new Goblin(new Point(1,1));
+        assertEquals(new Point(4,3), Fifteen.getClosestTargetInReadingOrder(g1,
+                Fifteen.findReachableTargets(g1,
+                        Fifteen.getInitialUnitPositions(Thirteen.readInput(input1, 9, 9)),
+                        Thirteen.readInput(input1, 9, 9)))
+        );
+    }
+
+    @Test
+    public void testGetNextPosition() {
+        assertEquals(new Point(3,4), Fifteen.getNextPosition(new Elf(new Point(4,4)), new Point(1, 4)));
     }
 }
