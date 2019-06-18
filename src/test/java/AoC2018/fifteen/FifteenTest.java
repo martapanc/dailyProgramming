d0d5aa34bf3bc11b9621d29765f0b18ad2a61214
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-06-18 15:36:59 +0100

    AoC 2018 D15: move accessory methods to separate class

--- src/test/java/AoC2018/fifteen/FifteenTest.java
+++ src/test/java/AoC2018/fifteen/FifteenTest.java
@@ -10,7 +10,8 @@ import java.util.stream.IntStream;
 
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FifteenTest {
 
@@ -44,7 +45,7 @@ class FifteenTest {
         unitList.add(new Goblin(new Point(4, 7)));
         unitList.add(new Goblin(new Point(7, 7)));
 
-        List<Unit> actual = Fifteen.getInitialUnitPositions(Thirteen.readInput(input1, 9,9));
+        List<Unit> actual = Utils.getInitialUnitPositions(Thirteen.readInput(input1, 9,9));
         IntStream.range(0, unitList.size()).forEach(i -> assertEquals(unitList.get(i).position, actual.get(i).position));
     }
 
@@ -58,7 +59,7 @@ class FifteenTest {
 
         assertEquals(pointList, Fifteen.findPossibleTargets(
                 new Goblin(new Point(1,1)),
-                Fifteen.getInitialUnitPositions(Thirteen.readInput(input1, 9, 9)),
+                Utils.getInitialUnitPositions(Thirteen.readInput(input1, 9, 9)),
                 Thirteen.readInput(input1, 9, 9)
         ));
     }
@@ -94,12 +95,12 @@ class FifteenTest {
 
         assertEquals(pointList, Fifteen.findReachableTargets(
                 new Elf(new Point(4,4)),
-                Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                Utils.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
                 Thirteen.readInput(input_flood, 9, 9)));
 
         assertEquals(new ArrayList<>(), Fifteen.findReachableTargets(
                 new Goblin(new Point(1,1)),
-                Fifteen.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
+                Utils.getInitialUnitPositions(Thirteen.readInput(input_flood, 9, 9)),
                 Thirteen.readInput(input_flood, 9, 9)));
     }
 
@@ -111,13 +112,13 @@ class FifteenTest {
 
         assertEquals(new Point(1, 5), Fifteen.getClosestTargetInReadingOrder(e1,
                 Fifteen.findReachableTargets(e1,
-                        Fifteen.getInitialUnitPositions(matrix1),
+                        Utils.getInitialUnitPositions(matrix1),
                         matrix1),
                 matrix1)
         );
         assertEquals(new Point(2, 7), Fifteen.getClosestTargetInReadingOrder(e2,
                 Fifteen.findReachableTargets(e2,
-                        Fifteen.getInitialUnitPositions(matrix1),
+                        Utils.getInitialUnitPositions(matrix1),
                         matrix1),
                 matrix1)
         );
@@ -128,13 +129,13 @@ class FifteenTest {
 
         assertEquals(new Point(4, 2), Fifteen.getClosestTargetInReadingOrder(e3,
                 Fifteen.findReachableTargets(e3,
-                        Fifteen.getInitialUnitPositions(matrix2),
+                        Utils.getInitialUnitPositions(matrix2),
                         matrix2),
                 matrix2)
         );
         assertEquals(new Point(4,3), Fifteen.getClosestTargetInReadingOrder(g1,
                 Fifteen.findReachableTargets(g1,
-                        Fifteen.getInitialUnitPositions(matrix2),
+                        Utils.getInitialUnitPositions(matrix2),
                         matrix2),
                 matrix2)
         );
@@ -160,15 +161,15 @@ class FifteenTest {
     @Test
     public void moveEverything() {
         char[][] matrix1 = Thirteen.readInput(input1, 9, 9);
-        Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix1), matrix1, 4);
+        Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix1), matrix1, 4);
 
-        assertEquals(24784, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix1), matrix1, 30));
+        assertEquals(24784, Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix1), matrix1, 30));
 
         char[][] matrix2 = Thirteen.readInput(input_progress, 7, 7);
-        assertEquals(27730, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix2), matrix2, 48));
+        assertEquals(27730, Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix2), matrix2, 48));
 
         char[][] matrix3 = Thirteen.readInput(input28, 7, 7);
-        Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix3), matrix3, 10);
+        Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix3), matrix3, 10);
 
 //        char[][] matrix4 = Thirteen.readInput(fight1, 7, 7);
 //        assertEquals(36334, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix4), matrix4, 40));
@@ -177,18 +178,18 @@ class FifteenTest {
 //        assertEquals(39514, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix5), matrix5, 50));
 
         char[][] matrix6 = Thirteen.readInput(fight3, 7, 7);
-        assertEquals(27755, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix6), matrix6, 40));
+        assertEquals(27755, Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix6), matrix6, 40));
 
         char[][] matrix7 = Thirteen.readInput(input2, 32, 32);
-        assertEquals(197538, Fifteen.moveEverything(Fifteen.getInitialUnitPositions(matrix7), matrix7, 1000));
+        assertEquals(197538, Fifteen.moveEverything(Utils.getInitialUnitPositions(matrix7), matrix7, 1000));
     }
 
     @Test
     public void canUnitAttackDirectly() {
         Unit e1 = new Elf(new Point(4,3));
         Unit g1 = new Goblin(new Point(4,2));
-        assertTrue(Fifteen.canUnitAttack(e1, Fifteen.getInitialUnitPositions(Thirteen.readInput(input_attackFirst, 9,9))));
-        assertTrue(Fifteen.canUnitAttack(g1, Fifteen.getInitialUnitPositions(Thirteen.readInput(input_attackFirst, 9,9))));
+        assertTrue(Fifteen.canUnitAttack(e1, Utils.getInitialUnitPositions(Thirteen.readInput(input_attackFirst, 9,9))));
+        assertTrue(Fifteen.canUnitAttack(g1, Utils.getInitialUnitPositions(Thirteen.readInput(input_attackFirst, 9,9))));
     }
 
     @Test
@@ -200,33 +201,33 @@ class FifteenTest {
 
         char[][] inputMatrix = Thirteen.readInput(input_bfs, 7, 7);
 
-        assertEquals(7, Fifteen.findMinDistanceWithBFS(g1, e1.position, inputMatrix));
-        assertEquals(7, Fifteen.findMinDistanceWithBFS(e1, g1.position, inputMatrix));
-        assertEquals(7, Fifteen.findMinDistanceWithBFS(g2, e1.position, inputMatrix));
-        assertEquals(7, Fifteen.findMinDistanceWithBFS(e1, g2.position, inputMatrix));
-        assertEquals(5, Fifteen.findMinDistanceWithBFS(g3, e1.position, inputMatrix));
-        assertEquals(5, Fifteen.findMinDistanceWithBFS(e1, g3.position, inputMatrix));
+        assertEquals(7, Utils.findMinDistanceWithBFS(g1, e1.position, inputMatrix));
+        assertEquals(7, Utils.findMinDistanceWithBFS(e1, g1.position, inputMatrix));
+        assertEquals(7, Utils.findMinDistanceWithBFS(g2, e1.position, inputMatrix));
+        assertEquals(7, Utils.findMinDistanceWithBFS(e1, g2.position, inputMatrix));
+        assertEquals(5, Utils.findMinDistanceWithBFS(g3, e1.position, inputMatrix));
+        assertEquals(5, Utils.findMinDistanceWithBFS(e1, g3.position, inputMatrix));
 
-        assertEquals(6, Fifteen.findMinDistanceWithBFS(g1, new Point(5,5), inputMatrix));
-        assertEquals(6, Fifteen.findMinDistanceWithBFS(g2, new Point(5,5), inputMatrix));
-        assertEquals(4, Fifteen.findMinDistanceWithBFS(g3, new Point(5,5), inputMatrix));
+        assertEquals(6, Utils.findMinDistanceWithBFS(g1, new Point(5,5), inputMatrix));
+        assertEquals(6, Utils.findMinDistanceWithBFS(g2, new Point(5,5), inputMatrix));
+        assertEquals(4, Utils.findMinDistanceWithBFS(g3, new Point(5,5), inputMatrix));
 
-        assertEquals(2, Fifteen.findMinDistanceWithBFS(g3, new Point(2,4), inputMatrix));
+        assertEquals(2, Utils.findMinDistanceWithBFS(g3, new Point(2,4), inputMatrix));
 
         Unit g4 = new Goblin(new Point(11, 11));
         Unit e2 = new Elf(new Point(19, 26));
 
-        assertEquals(27, Fifteen.findMinDistanceWithBFS(g4, e2.position, Thirteen.readInput(input2, 32,32)));
-        assertEquals(27, Fifteen.findMinDistanceWithBFS(e2, g4.position, Thirteen.readInput(input2, 32,32)));
+        assertEquals(27, Utils.findMinDistanceWithBFS(g4, e2.position, Thirteen.readInput(input2, 32,32)));
+        assertEquals(27, Utils.findMinDistanceWithBFS(e2, g4.position, Thirteen.readInput(input2, 32,32)));
     }
 
     @Test
     public void testGetDistance() {
         char[][] matrix = Thirteen.readInput(input_bfs, 7, 7);
 
-        assertEquals(9, Fifteen.getDistanceWithBFS(new Point(5,1), new Point(5,4), matrix));
-        assertEquals(9, Fifteen.getDistanceWithBFS(new Point(5,4), new Point(5,1), matrix));
-        assertEquals(5, Fifteen.getDistanceWithBFS(new Point(2,1), new Point(1,5), matrix));
-        assertEquals(4, Fifteen.getDistanceWithBFS(new Point(1,1), new Point(1,5), matrix));
+        assertEquals(9, Utils.getDistanceWithBFS(new Point(5,1), new Point(5,4), matrix));
+        assertEquals(9, Utils.getDistanceWithBFS(new Point(5,4), new Point(5,1), matrix));
+        assertEquals(5, Utils.getDistanceWithBFS(new Point(2,1), new Point(1,5), matrix));
+        assertEquals(4, Utils.getDistanceWithBFS(new Point(1,1), new Point(1,5), matrix));
     }
 }
