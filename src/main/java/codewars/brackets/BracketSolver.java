dec2899b7f0805f1385a342e920af0aee6564a95
Marta Pancaldi <marta.panc@gmail.com>
2019-11-07 18:51:39 +0000

    Add function and test for bracket solver

--- src/main/java/codewars/brackets/BracketSolver.java
+++ src/main/java/codewars/brackets/BracketSolver.java
@@ -1,8 +1,15 @@
 package codewars.brackets;
 
+import java.util.Stack;
+
 public class BracketSolver {
 
     public static boolean areBracketsValid(String input) {
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+
+        }
         return false;
     }
 }
