3a2925f41ffc54f7d5100c0ea0cc90fc91cf8a72
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-11-15 09:31:38 +0000

    Add Squared array algorithm

---
+++ src/main/java/codewars/squaredArray/SquaredArray.java
@@ -0,0 +1,16 @@
+package codewars.squaredArray;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SquaredArray {
+
+    public static boolean comp(int[] a, int[] b) {
+        if (a != null & b != null && a.length == b.length) {
+            Arrays.sort(a);
+            Arrays.sort(b);
+            return IntStream.range(0, a.length).noneMatch(i -> b[i] != Math.pow(a[i], 2));
+        }
+        return false;
+    }
+}
