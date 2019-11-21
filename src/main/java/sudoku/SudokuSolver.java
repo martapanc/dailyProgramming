eba03ebd81f946bb832b7c681c450df372ab3a94
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-11-21 12:45:06 +0000

    Move constant inside method

--- src/main/java/sudoku/SudokuSolver.java
+++ src/main/java/sudoku/SudokuSolver.java
@@ -37,8 +37,6 @@ public class SudokuSolver {
 
     */
 
-    private static final int horLines = 25;
-
     static String solveSudoku(String input) {
 
         // Use LinkedHashedMap to keep the insertion order (left-right, top-bottom)
@@ -180,8 +178,9 @@ public class SudokuSolver {
 
     static void printSudoku(String input) {
         char[] chars = input.toCharArray();
+        final int COUNT = 25;
 
-        System.out.println("-".repeat(horLines));
+        System.out.println("-".repeat(COUNT));
         for (int i = 0; i < chars.length; i++) {
             char num = chars[i];
             if (i % 9 == 0) {
@@ -195,7 +194,7 @@ public class SudokuSolver {
                 System.out.println();
             }
             if (i % 27 == 26) {
-                System.out.println("-".repeat(25));
+                System.out.println("-".repeat(COUNT));
             }
         }
     }
