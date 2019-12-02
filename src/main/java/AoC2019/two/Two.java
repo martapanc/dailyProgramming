f968aac2a27e7542dd26478203432a4ae3e46cd6
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-12-02 10:57:35 +0000

    Add AoC2019 Day 2 Part 1

--- src/main/java/AoC2019/two/Two.java
+++ src/main/java/AoC2019/two/Two.java
@@ -1,4 +1,48 @@
 package AoC2019.two;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Two {
+
+    public static ArrayList<Integer> readInput(String input) {
+        ArrayList<Integer> list = new ArrayList();
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(input));
+            String line = reader.readLine();
+            while (line != null) {
+                Arrays.stream(line.split(",")).map(Integer::parseInt).forEachOrdered(list::add);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static int processInput(String input) {
+        ArrayList<Integer> numbers = readInput(input);
+        for (int i = 0; i < numbers.size(); i += 4) {
+            Integer key = numbers.get(i);
+            if (key == 99) {
+                break;
+            }
+            Integer input1Pos = numbers.get(i + 1);
+            Integer input2Pos = numbers.get(i + 2);
+            Integer outputPos = numbers.get(i + 3);
+            if (key == 1) {
+                numbers.set(outputPos, numbers.get(input1Pos) + numbers.get(input2Pos));
+            }
+            if (key == 2) {
+                numbers.set(outputPos, numbers.get(input1Pos) * numbers.get(input2Pos));
+            }
+        }
+
+        return numbers.get(0);
+    }
 }
--- src/test/java/AoC2019/two/TwoTest.java
+++ src/test/java/AoC2019/two/TwoTest.java
@@ -1,5 +1,28 @@
 package AoC2019.two;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TwoTest {
 
+    private static final String INPUT1 = "src/test/java/AoC2019/two/input1";
+    private static final String INPUT2 = "src/test/java/AoC2019/two/input2";
+    private static final String INPUT3 = "src/test/java/AoC2019/two/input3";
+    private static final String INPUT4 = "src/test/java/AoC2019/two/input4";
+    private static final String INPUT5 = "src/test/java/AoC2019/two/input5";
+
+    @Test
+    public void testReadInput() {
+        System.out.println(Two.readInput(INPUT1));
+    }
+
+    @Test
+    public void processInput() {
+        assertEquals(3500, Two.processInput(INPUT2));
+        assertEquals(2, Two.processInput(INPUT3));
+        assertEquals(2, Two.processInput(INPUT4));
+        assertEquals(30, Two.processInput(INPUT5));
+        assertEquals(5866714, Two.processInput(INPUT1));
+    }
 }
