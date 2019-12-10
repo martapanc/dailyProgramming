e908269173288879b3bb6b55f759eaa328bd221a
Marta Pancaldi <marta.panc@gmail.com>
2019-12-10 16:09:29 +0000

    Add AoC2019 Day 5 part 2 complete

--- src/test/java/AoC2019/five/FiveTest.java
+++ src/test/java/AoC2019/five/FiveTest.java
@@ -11,6 +11,12 @@ public class FiveTest {
 
     private static final String INPUT1 = "src/test/java/AoC2019/five/input1";
     private static final String INPUT2 = "src/test/java/AoC2019/five/input2";
+    private static final String INPUT2_2 = "src/test/java/AoC2019/five/input2_2";
+    private static final String INPUT3 = "src/test/java/AoC2019/five/input3";
+    private static final String INPUT3_2 = "src/test/java/AoC2019/five/input3_2";
+    private static final String INPUT4 = "src/test/java/AoC2019/five/input4";
+    private static final String INPUT5 = "src/test/java/AoC2019/five/input5";
+    private static final String INPUT6 = "src/test/java/AoC2019/five/input6";
 
     @Test
     public void testReadInput() {
@@ -23,6 +29,30 @@ public class FiveTest {
 
     @Test
     public void testProcessInput() {
-        assertEquals(7988899, Five.processInput(Two.readInput(INPUT1)));
+        assertEquals(7988899, Five.processInput(Two.readInput(INPUT1), 1));
+        assertEquals(13758663, Five.processInput(Two.readInput(INPUT1), 5));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT2), 7));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT2), 8));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT2_2), 8));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT2_2), 7));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT3), 7));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT3), 8));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT3_2), 8));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT3_2), 7));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT4), 0));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT4), 2));
+
+        assertEquals(0, Five.processInput(Two.readInput(INPUT5), 0));
+        assertEquals(1, Five.processInput(Two.readInput(INPUT5), 2));
+
+        assertEquals(999, Five.processInput(Two.readInput(INPUT6), 7));
+        assertEquals(1000, Five.processInput(Two.readInput(INPUT6), 8));
+        assertEquals(1001, Five.processInput(Two.readInput(INPUT6), 9));
+
     }
 }
