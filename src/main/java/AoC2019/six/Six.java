8ba642c9efe8a6d437bff777dd4f203a33f96932
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-12-13 13:50:36 +0000

    AoC2019 D06P1: complete

--- src/main/java/AoC2019/six/Six.java
+++ src/main/java/AoC2019/six/Six.java
@@ -4,7 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Six {
 
@@ -24,5 +29,52 @@ public class Six {
             e.printStackTrace();
         }
         return list;
+    }
+
+    static Map<String, Set<String>> getOrbitMap(List<OrbitSystem> list) {
+        Map<String, Set<String>> orbitMap = new HashMap<>();
+
+        list.forEach(orbitSystem -> {
+            String center = orbitSystem.getCenter();
+            String satellite = orbitSystem.getSatellite();
+            if (orbitMap.containsKey(center)) {
+                orbitMap.get(center).add(satellite);
+            } else {
+                Set<String> satellites = new HashSet<>();
+                satellites.add(satellite);
+                orbitMap.put(center, satellites);
+            }
+        });
+
+        return orbitMap;
+    }
+
+    static Map<String, Set<String>> getCompleteOrbitMap(List<OrbitSystem> list) {
+        Map<String, Set<String>> orbitMap = getOrbitMap(list);
+        int satelliteListsTotalLength = getListSizeSum(orbitMap);
+        int tempTotalLength = 0;
+
+        while (tempTotalLength != satelliteListsTotalLength) {
+            satelliteListsTotalLength = getListSizeSum(orbitMap);
+
+            Map<String, Set<String>> newOrbitMap = orbitMap.entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, entry -> new HashSet<>(entry.getValue()), (a, b) -> b));
+
+            for (Map.Entry<String, Set<String>> entry : orbitMap.entrySet()) {
+                for (String sat : entry.getValue()) {
+                    if (newOrbitMap.containsKey(sat)) {
+                        newOrbitMap.get(entry.getKey()).addAll(orbitMap.get(sat));
+                    }
+                }
+            }
+            orbitMap = newOrbitMap;
+            tempTotalLength = getListSizeSum(orbitMap);
+        }
+
+        return orbitMap;
+    }
+
+    static int getListSizeSum(Map<String, Set<String>> orbitMap) {
+        return orbitMap.values().stream().mapToInt(Set::size).sum();
     }
 }
--- src/test/java/AoC2019/six/SixTest.java
+++ src/test/java/AoC2019/six/SixTest.java
@@ -2,6 +2,11 @@ package AoC2019.six;
 
 import org.junit.Test;
 
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
 public class SixTest {
 
     private static final String INPUT1 = "src/test/java/AoC2019/six/input1";
@@ -11,5 +16,21 @@ public class SixTest {
     public void testReadInput() {
         System.out.println(Six.readInput(INPUT1));
         System.out.println(Six.readInput(INPUT2));
+    }
+
+    @Test
+    public void testGetOrbitMap() {
+        System.out.println(Six.getOrbitMap(Six.readInput(INPUT1)));
+    }
+
+    @Test
+    public void testGetCompleteOrbitMap() {
+        Map<String, Set<String>> completeOrbitMap = Six.getCompleteOrbitMap(Six.readInput(INPUT1));
+        System.out.println(completeOrbitMap);
+        assertEquals(42, Six.getListSizeSum(completeOrbitMap));
+
+        Map<String, Set<String>> completeOrbitMap2 = Six.getCompleteOrbitMap(Six.readInput(INPUT2));
+        System.out.println(completeOrbitMap2);
+        assertEquals(119831, Six.getListSizeSum(completeOrbitMap2));
     }
 }
