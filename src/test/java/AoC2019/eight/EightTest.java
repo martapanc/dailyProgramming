70f9308fb301d17b0307a74d52df2d9bf18a191b
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2019-12-31 13:50:36 +0000

    AoC2019 D09P1: setup from D05

--- src/test/java/AoC2019/eight/EightTest.java
+++ src/test/java/AoC2019/eight/EightTest.java
@@ -9,6 +9,7 @@ public class EightTest {
     private static String INPUT1 = "src/test/java/AoC2019/eight/input1";
     private static String INPUT2 = "src/test/java/AoC2019/eight/input2";
     private static String INPUT3 = "src/test/java/AoC2019/eight/input3";
+    private static String INPUT4 = "src/test/java/AoC2019/eight/input4";
 
     @Test
     public void testReadInput() {
@@ -20,6 +21,7 @@ public class EightTest {
     @Test
     public void getBestLayer() {
         assertEquals(2480, Eight.getBestLayer(Eight.readInput(INPUT1, 25, 6)));
+        assertEquals(2016, Eight.getBestLayer(Eight.readInput(INPUT4, 25, 6)));
     }
 
     @Test
@@ -28,5 +30,8 @@ public class EightTest {
         assertEquals("11110100011110010000100100001010001100101000010010001000101011100100001111001000" +
                         "0010010010100001001010000001001001010000100101111000100111001111010010",
                 Eight.getRenderedImage(Eight.readInput(INPUT1, 25, 6), 25, 6));
+        assertEquals("10010111100110011110100101001000010100100001010010111100010010000001001001010010" +
+                        "0100010000010001001010010100001001010000100101001011110011001111001100",
+                Eight.getRenderedImage(Eight.readInput(INPUT4, 25, 6), 25, 6));
     }
 }
---
+++ src/test/java/AoC2019/nine/NineTest.java
@@ -0,0 +1,7 @@
+package AoC2019.nine;
+
+import static org.junit.Assert.*;
+
+public class NineTest {
+
+}
