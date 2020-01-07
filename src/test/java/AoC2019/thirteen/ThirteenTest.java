818e9412c8dc41c7ca0c64d840dac01012d4478d
Marta Pancaldi <marta.panc@gmail.com>
2020-01-07 22:32:04 +0000

    AoC2019 D13P2: setup testing

--- src/test/java/AoC2019/thirteen/ThirteenTest.java
+++ src/test/java/AoC2019/thirteen/ThirteenTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertEquals;
 public class ThirteenTest {
 
     private static final String INPUT1 = "src/test/java/AoC2019/thirteen/input1";
+    private static final String INPUT2 = "src/test/java/AoC2019/thirteen/input2";
 
     @Test
     public void testReadInput() {
@@ -25,11 +26,12 @@ public class ThirteenTest {
 
     @Test
     public void testCountTilesOfType() {
-        assertEquals(357, Thirteen.countTilesOfType(Tile.BLOCK, Thirteen.processInput(Nine.readInput(INPUT1))));
+        System.out.println(Thirteen.processInput(Nine.readInput(INPUT2)));
     }
 
     @Test
     public void testPrintTileMap() {
         Thirteen.printTileMap(Thirteen.processInput(Nine.readInput(INPUT1)));
+        Thirteen.printTileMap(Thirteen.processInput(Nine.readInput(INPUT2)));
     }
 }
