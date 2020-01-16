6d383991e0287a6df618a1ff4d5efa1bb193cecc
Marta Pancaldi <MartaPancaldi@rentalcars.com>
2020-01-16 16:41:31 +0000

    AoC2019 D16P1: write multiply function

--- src/main/java/AoC2019/sixteen/Sixteen.java
+++ src/main/java/AoC2019/sixteen/Sixteen.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Sixteen {
     public static List<Integer> readInput(String input) {
@@ -35,5 +36,30 @@ public class Sixteen {
         List<Integer> list = new ArrayList<>();
         addNumbersToList(list, numbers);
         return list;
+    }
+
+    public static int computeNthDigit(List<Integer> numbers, int n) {
+        int[] pattern = new int[]{0, 1, 0, -1};
+        List<Integer> repeatedPattern = new ArrayList<>();
+        int count = 1;
+        int index = 0;
+
+        int i = 0;
+        while (i++ < numbers.size() + 1) {
+            index = index % 4;
+            repeatedPattern.add(pattern[index]);
+            if (count >= n) {
+                index++;
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        repeatedPattern.remove(0);
+        return getOnesDigitOfComputation(numbers, repeatedPattern);
+    }
+
+    private static int getOnesDigitOfComputation(List<Integer> numbers, List<Integer> pattern) {
+        return Math.abs(IntStream.range(0, numbers.size()).map(i -> numbers.get(i) * pattern.get(i)).sum() % 10);
     }
 }
--- src/test/java/AoC2019/sixteen/SixteenTest.java
+++ src/test/java/AoC2019/sixteen/SixteenTest.java
@@ -2,10 +2,14 @@ package AoC2019.sixteen;
 
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class SixteenTest {
 
     private static final String INPUT1 = "src/test/java/AoC2019/sixteen/input1";
     private static final String INPUT2 = "12345678";
+    private static final String INPUT3 = "48226158";
 
     @Test
     public void testReadInput() {
@@ -13,4 +17,15 @@ public class SixteenTest {
         System.out.println(Sixteen.readStringInput(INPUT2));
     }
 
+    @Test
+    public void testComputeNthDigit() {
+        assertEquals(8, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 2));
+        assertEquals(4, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 1));
+        assertEquals(2, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 3));
+        assertEquals(2, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 4));
+        assertEquals(6, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 5));
+        assertEquals(1, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 6));
+        assertEquals(5, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 7));
+        assertEquals(8, Sixteen.computeNthDigit(Sixteen.readStringInput(INPUT2), 8));
+    }
 }
